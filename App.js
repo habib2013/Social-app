bed07473ff6716637fe43fa09727c90eab671f0e
habib2013 <nairabet25@gmail.com>
2020-06-16 17:48:18 +0100

    first Commit

--- App.js
+++ App.js
@@ -10,7 +10,20 @@ import LoginScreen from './Screens/loginScreen';
 import HomeScreen from './Screens/homeScreen';
 import * as firebase from 'firebase';
 
+var firebaseConfig = {
+  apiKey: "AIzaSyAtMrui49AfIVhQmHCaKpnoYATLcpgGn8k",
+  authDomain: "social-app-a0f46.firebaseapp.com",
+  databaseURL: "https://social-app-a0f46.firebaseio.com",
+  projectId: "social-app-a0f46",
+  storageBucket: "social-app-a0f46.appspot.com",
+  messagingSenderId: "668057381690",
+  appId: "1:668057381690:web:8cb0e52cecd57181e63e03",
+  measurementId: "G-T3SMV9828X"
+};
 
+// checking it
+
+firebase.initializeApp(firebaseConfig);
 
 const AppStack = createStackNavigator({
   Home: HomeScreen
